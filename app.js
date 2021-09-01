92ebf0c64230044a5bae9e68a0c7b8b89c34d77e
iuryliberato <iuryliberato@hotmail.com>
2021-09-01 13:56:09 +0100

    added second and third character, added

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ function init() {
   // Elements 
 
   const cells = document.querySelectorAll('.cell')
-  
+
   const audio = document.querySelector('#backgroundSong')
   const moving = document.querySelector('#moving_effect')
   const carHorn = document.querySelector('#carHorn')
-  
+
   const seagullSound = document.querySelector('#seagullSound')
   const winSound = document.querySelector('#win')
   const gameOverTheme = document.querySelector('#gameOverTheme')
@@ -19,7 +19,7 @@ function init() {
   const carGap = 3
   const carStartIndex = width * carRowIndex
   const cars = Array.from(cells).slice(carStartIndex, carStartIndex + width)
- 
+
   // seagull 
   const seagullIndex = 2
   const seagullGap = 3
@@ -34,16 +34,30 @@ function init() {
 
   const popGameOver = document.querySelector('.gameOverOverlay')
   const popWin = document.querySelector('.winOverlay')
-  const startingPosition = 86
-  let characterPosition = startingPosition
+ 
+  const startingPosition1 = 86
+  const startingPosition2 = 82
+  const startingPosition3 = 78
+
+  let activeCharacter = 1
+
+  //Object
+
+  const characterPositions = {
+    1: startingPosition1,
+    2: startingPosition2,
+    3: startingPosition3
+  }
 
   //Functions
   function carHornPlay() {
     carHorn.play()
   }
+
   function movesound() {
     moving.play()
   }
+
   function seagullSounds() {
     seagullSound.play()
     setTimeout(() => {
@@ -51,9 +65,11 @@ function init() {
     }, 4000
     )
   }
+
   function winTheme() {
     winSound.play()
   }
+
   function gameOverThemee() {
     gameOverTheme.currentTime = 0
     gameOverTheme.play()
@@ -61,19 +77,27 @@ function init() {
 
   function startReset() {
     popUps.forEach(popUp => popUp.classList.remove('active'))
-    characterPosition = startingPosition
+  
+    characterPositions[1] = startingPosition1
+    characterPositions[2] = startingPosition2
+    characterPositions[3] = startingPosition3
     moveCharacter()
     audio.play()
     winSound.pause()
     gameOverTheme.pause()
-    gameInterval = setInterval(run, 600)
+    setSpeed(450)
+    activeCharacter = 1
   }
-
+  function setSpeed(ms) {
+    clearInterval(gameInterval)
+    gameInterval = setInterval(run, ms)
+  }
 
   function moveCharacter() {
     cells.forEach(cell => cell.innerHTML = '')
-    cells[characterPosition].innerHTML = '<img src="/surfer.png" class="surfer" >'
-
+    cells[characterPositions[1]].innerHTML = '<img src="/surfer.png" class="surfer" >'
+    cells[characterPositions[2]].innerHTML = '<img src="/surfer2.png" class="surfer" >'
+    cells[characterPositions[3]].innerHTML = '<img src="/surfer3.png" class="surfer" >'
   }
 
   function gameOver() {
@@ -83,10 +107,11 @@ function init() {
   function win() {
     popWin.classList.add('active')
     stopGame()
+    winTheme()
   }
   function stopGame() {
     clearInterval(gameInterval)
-   audio.pause()
+    audio.pause()
   }
 
 
@@ -98,7 +123,7 @@ function init() {
       if (index % carGap === carGap - 1 - counter % carGap) {
         car.classList.add('car')
 
-        if (index + width * carRowIndex === characterPosition) {
+        if (index + width * carRowIndex === characterPositions[activeCharacter]) {
           carHornPlay()
           gameOver()
 
@@ -109,7 +134,7 @@ function init() {
       if (index % seagullGap === seagullGap - 1 - counter % seagullGap) {
         seagull.classList.add('seagull')
 
-        if (index + width * seagullIndex === characterPosition) {
+        if (index + width * seagullIndex === characterPositions[activeCharacter]) {
           gameOver()
           seagullSounds()
         }
@@ -121,7 +146,7 @@ function init() {
         truck.classList.add('truck')
 
 
-        if (index + width * truckRowIndex === characterPosition) {
+        if (index + width * truckRowIndex === characterPositions[activeCharacter]) {
           carHornPlay()
           gameOver()
         }
@@ -131,22 +156,22 @@ function init() {
   }
 
   function keyUp(event) {
-    let newPosition = characterPosition
+    let newPosition = characterPositions[activeCharacter]
 
-    if (event.code === 'ArrowUp' && characterPosition >= width) {
+    if (event.code === 'ArrowUp' && characterPositions[activeCharacter] >= width) {
       newPosition -= width
 
-    } else if (event.code === 'ArrowDown' && characterPosition < cells.length - width) {
+    } else if (event.code === 'ArrowDown' && characterPositions[activeCharacter] < cells.length - width) {
       newPosition += width
 
-    } else if (event.code === 'ArrowLeft' && characterPosition % width !== 0) {
+    } else if (event.code === 'ArrowLeft' && characterPositions[activeCharacter] % width !== 0) {
       newPosition--
-    } else if (event.code === 'ArrowRight' && (characterPosition + 1) % width !== 0) {
+    } else if (event.code === 'ArrowRight' && (characterPositions[activeCharacter] + 1) % width !== 0) {
       newPosition++
     }
 
     if (!cells[newPosition].classList.contains('no-Entry')) {
-      characterPosition = newPosition
+      characterPositions[activeCharacter] = newPosition
       movesound()
 
     }
@@ -166,9 +191,18 @@ function init() {
       gameOver()
       carHornPlay()
     }
-    if (characterPosition <= width) {
-      win()
-      winTheme()
+    if (characterPositions[activeCharacter] <= width) {
+      if (activeCharacter === 1) {
+        activeCharacter = 2
+        setSpeed(350)
+      } else if (activeCharacter === 2) {
+        activeCharacter = 3
+        setSpeed(250)
+      } else {
+        win()
+      }
+
+
     }
     moveCharacter()
   }
